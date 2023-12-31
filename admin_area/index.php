0d7ce9a907077d42adab931391d42d98446a75b6
Andhika Eka PS <95490208+AndhikaEkaPS@users.noreply.github.com>
2023-12-31 17:39:06 +0700

    Add files via upload

---
+++ admin_area/index.php
@@ -0,0 +1,129 @@
+<!-- connect file -->
+<?php
+include('../includes/connect.php');
+include('../functions/common_function.php');
+session_start();
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Admin Dashboard</title>
+    <!-- bootsrap css link -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+
+    <!-- font awesome link -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+
+    <!-- css file -->
+    <link rel="stylesheet" href="../style.css">
+    <style>
+        .admin_image {
+            width: 100px;
+            object-fit: contain;
+        }
+
+        .footer {
+            position: absolute;
+            bottom: 0;
+        }
+
+        body {
+            overflow-x: hidden;
+        }
+
+        .product_img {
+            width: 10%;
+            object-fit: contain;
+        }
+    </style>
+</head>
+
+<body>
+    <!-- navbar -->
+    <div class="container-fluid p-0">
+        <!-- first child -->
+        <nav class="navbar navbar-expand-lg navbar-light bg-info">
+            <div class="container-fluid">
+                <img src="../img/logo.1.png" alt="" class="logo">
+                <nav class="navbar navbar-expand-lg">
+                    <ul class="navbar nav">
+                    <!-- second child -->
+                    <nav class="navbar navbar-expand-lg navbar-dark bg-secondary">
+                        <ul class="navbar-nav me-auto">
+                        <?php
+                        if (!isset($_SESSION['username'])) {
+                            echo "<li class='nav-item'><a class='nav-link' href='#'>Welcome Guest</a></li>";
+                        } else {
+                            echo "<li class='nav-item'><a class='nav-link' href='#'>Welcome " . $_SESSION['username'] . "</a></li>";
+                        }
+                        if (!isset($_SESSION['username'])) {
+                            echo "<li class='nav-item'><a class='nav-link' href='./admin_area/admin_login.php'>Login</a></li>";
+                        } else {
+                            echo "<li class='nav-item'><a class='nav-link' href='./admin_area/logout.php'>Logout</a></li>";
+                        }
+                        ?>
+                    </ul>
+                </nav>
+            </div>
+        </nav>
+
+        <!-- second child -->
+        <div class="bg-light">
+            <h3 class="text-center p-2">Manage Details</h3>
+        </div>
+
+        <!-- third child -->
+        <div class="row">
+            <div class="col-md-12 bg-secondary p-1 d-flex align-items-center">
+                <div class="p-3">
+                    <a href="#"><img src="../img/products/e7.png" alt="" class="admin_image"></a>
+                    <p class="text-light text-center">Admin Name</p>
+                </div>
+                <div class="button text-center">
+                    <button class="my-3"><a href="insert_product.php" class="nav-link text-light bg-info my-1">Insert Products</a></button>
+                    <button><a href="index.php?view_products" class="nav-link text-light bg-info my-1">View Products</a></button>
+                    <button><a href="index.php?insert_category" class="nav-link text-light bg-info my-1">Insert Categories</a></button>
+                    <button><a href="" class="nav-link text-light bg-info my-1">View Categories</a></button>
+                    <button><a href="index.php?insert_brand" class="nav-link text-light bg-info my-1">Insert Brands</a></button>
+                    <button><a href="" class="nav-link text-light bg-info my-1">View Brands</a></button>
+                    <button><a href="" class="nav-link text-light bg-info my-1">All Orders</a></button>
+                    <button><a href="" class="nav-link text-light bg-info my-1">All Payments</a></button>
+                    <button><a href="" class="nav-link text-light bg-info my-1">List Users</a></button>
+                    <button><a href="" class="nav-link text-light bg-info my-1">Log Out</a></button>
+                </div>
+            </div>
+        </div>
+
+        <!-- fourth child -->
+        <div class="container my-3">
+            <?php
+            if (isset($_GET['insert_category'])) {
+                include('insert_categories.php');
+            }
+            if (isset($_GET['insert_brand'])) {
+                include('insert_brands.php');
+            }
+            if (isset($_GET['view_products'])) {
+                include('view_products.php');
+            }
+            ?>
+        </div>
+
+        <!-- last child -->
+        <!-- include footer -->
+        <?php
+        include("../includes/footer.php")
+        ?>
+    </div>
+
+
+    <!-- bootsrap js link -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.min.js" integrity="sha384-BBtl+eGJRgqQAUMxJ7pMwbEyER4l1g+O15P+16Ep7Q9Q+zqX6gSbd85u4mG4QzX+" crossorigin="anonymous"></script>
+</body>
+
+</html>
